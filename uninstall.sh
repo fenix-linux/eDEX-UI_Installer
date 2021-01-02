e4047f979312928c2510001de546b47c16bd718f
fenixlinuxos <66138813+fenixlinuxos@users.noreply.github.com>
2021-01-02 05:01:12 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd
-rm -rf uninstall.sh* &>/dev/null
 rm -rf eDEX-UI-Linux-x86_64.AppImage &>/dev/null
 rm -rf ~/.local/share/icons/edex.png &>/dev/null &>/dev/null
 rm -rf ~/.local/share/applications/edex-ui-2.2.5.desktop &>/dev/null
